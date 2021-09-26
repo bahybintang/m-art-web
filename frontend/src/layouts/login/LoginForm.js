2e123306d36d3d58b0ad939450fb25378d4a265d
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-09-26 20:17:25 +0700

    Add seller buyer selector

--- frontend/src/layouts/login/LoginForm.js
+++ frontend/src/layouts/login/LoginForm.js
@@ -5,27 +5,51 @@ import {
   FormLabel,
   Input,
   Stack,
+  useRadioGroup,
+  HStack,
 } from '@chakra-ui/react';
-import * as React from 'react';
+import React, { useState } from 'react';
 import { PasswordField } from './PasswordField';
+import RadioCard from './RadioCard';
 
-export const LoginForm = props => (
-  <chakra.form
-    onSubmit={e => {
-      e.preventDefault();
-      // your login logic here
-    }}
-    {...props}
-  >
-    <Stack spacing="6">
-      <FormControl id="email">
-        <FormLabel>Email address</FormLabel>
-        <Input name="email" type="email" autoComplete="email" required />
-      </FormControl>
-      <PasswordField />
-      <Button type="submit" colorScheme="blue" size="lg" fontSize="md">
-        Sign in
-      </Button>
-    </Stack>
-  </chakra.form>
-);
+export const LoginForm = props => {
+  const options = ['Seller', 'Buyer'];
+  const [role, setRole] = useState(null);
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'framework',
+    defaultValue: 'react',
+    onChange: setRole,
+  });
+
+  const group = getRootProps();
+  return (
+    <chakra.form
+      onSubmit={e => {
+        e.preventDefault();
+        // your login logic here
+      }}
+      {...props}
+    >
+      <Stack spacing="6">
+        <FormControl id="email">
+          <FormLabel>Email address</FormLabel>
+          <Input name="email" type="email" autoComplete="email" required />
+        </FormControl>
+        <PasswordField />
+        <HStack {...group}>
+          {options.map(value => {
+            const radio = getRadioProps({ value });
+            return (
+              <RadioCard key={value} {...radio}>
+                {value}
+              </RadioCard>
+            );
+          })}
+        </HStack>
+        <Button type="submit" colorScheme="blue" size="lg" fontSize="md">
+          Sign in
+        </Button>
+      </Stack>
+    </chakra.form>
+  );
+};
