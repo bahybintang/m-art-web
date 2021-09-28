3c6ebd379f4a3c835810bde93d3762135af66f2a
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-09-28 21:22:36 +0700

    Add profile page

--- frontend/src/layouts/seller/Profile.js
+++ frontend/src/layouts/seller/Profile.js
@@ -1,5 +1,25 @@
-function Profile(props) {
-  return <div>Profile</div>;
+import { ProfileForm } from './ProfileForm';
+import { Box, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+import { Card } from './Card';
+
+function Profile() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'inherit')}
+      minH="100vh"
+      py="12"
+      px={{ base: '4', lg: '8' }}
+    >
+      <Box maxW="md" mx="auto">
+        <Heading textAlign="center" size="xl" fontWeight="extrabold">
+          Akun Anda
+        </Heading>
+        <Card>
+          <ProfileForm />
+        </Card>
+      </Box>
+    </Box>
+  );
 }
 
 export default Profile;
