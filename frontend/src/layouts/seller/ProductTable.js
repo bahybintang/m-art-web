a47d5b405bb0c7a42a3d58ca9c0e492523d630e2
vido <perdo@mamiteam.com>
2021-10-14 21:44:27 +0700

    Add Delete Confirmation

--- frontend/src/layouts/seller/ProductTable.js
+++ frontend/src/layouts/seller/ProductTable.js
@@ -9,12 +9,22 @@ import {
   Image,
   Button,
   Grid,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
 } from '@chakra-ui/react';
 import { useHistory } from 'react-router-dom';
 
 function ProductTable(props) {
   const history = useHistory();
   const { products } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <Table variant="simple">
       <Thead>
@@ -51,12 +61,29 @@ function ProductTable(props) {
             <Td>
               <Grid>
                 <Button colorScheme="blue">Edit</Button>
-                <Button colorScheme="red">Delete</Button>
+                <Button onClick={onOpen} colorScheme="red">Delete</Button>
               </Grid>
             </Td>
           </Tr>
         ))}
       </Tbody>
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Confirmation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <p>Are you sure you want to delete this item?</p>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" mr={3}>
+              Delete
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Table>
   );
 }
